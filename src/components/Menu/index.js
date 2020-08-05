81ac3b2a6172ae7f895e4d59f92cb5c37788b60e
Nathalya Silva <nathalya@buscaprev.com.br>
2020-08-04 22:13:43 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import Button from '../Button';
+import './Menu.css';
+
+import {
+    Link
+  } from 'react-router-dom';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <Link to='/'>
+                <img className="Logo" src={Logo} alt="Natflix logo"/>
+            </Link>
+
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo Vídeo
+            </Button>
+        </nav>
+    )
+}
+
+export default Menu;
